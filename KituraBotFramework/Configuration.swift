ef1dbf584c20c3af20b10498c8bf25f94626b6af
Jacopo Mangiavacchi <jacopo.mangiavacchi@uk.ibm.com>
2016-10-07 17:49:33 +0100

    Update Configuration.swift

--- KituraBotFramework/Configuration.swift
+++ KituraBotFramework/Configuration.swift
@@ -12,14 +12,14 @@ public struct Configuration {
     public static let appName = "KBot"
     static let sharedGroup = "group.jacopomangiavacchi.KituraBot"
     
-    static let apiUrl = "https://jswiftbot.mybluemix.net/mobileapi"
+    static let apiUrl = "https://xxx/mobileapi"
     
     // Token to use for verifing access to the Mobile API
     static let mobileApiSecurityToken = "1234"
 
     // Bluemix Push configuration
     public static let bluemixRegion = ".ng.bluemix.net"
-    public static let appGUID = "e34a187c-a0d2-47e4-b558-c7dc9b45dfbd"
-    public static let clientSecret = "cfab758b-8a43-4773-bf9e-9ae37cc8d8cc"
+    public static let appGUID = "xxx"
+    public static let clientSecret = "xxx"
 }
 
